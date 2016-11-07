86f53600d17970b8ed5facb6b50c72c97f109f79
dehartjw <dehartjw@gmail.com>
2016-11-07 05:08:16 -0500

    Create Ugly.java

---
+++ Ugly.java
@@ -0,0 +1,16 @@
+/*
+@Author
+//Andrew DeHart
+//AMSA Charter School
+//JAVA Class
+*/
+
+public class Ugly { 
+  public static void main(String[] args) { 
+    System.out.println("Why am I so ugly?"); 
+    System.out.println("Is it because of my beard?"); 
+    System.out.println("That can not be it, I keep it shiny with bacon grease.");
+    System.out.println("Could it be my eyes?"); 
+    System.out.println("Silly me, I just had to put on my glasses!");
+  } 
+}
